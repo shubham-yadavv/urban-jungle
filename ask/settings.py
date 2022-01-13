09c41939969d02e068e75127daad91c33092457a
shubham <shubham22121@gmail.com>
2022-01-13 22:57:47 +0530

    modified files and added dockerfile

--- ask/settings.py
+++ ask/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tirals.trials@gmail.com'
-EMAIL_HOST_PASSWORD = '****'
+EMAIL_HOST_PASSWORD = 'Trials321'
